58c25c7359a53a2e58c9b7154a19307b0bef3845
pierre-jannot <pierre.jannot@isen.yncrea.fr>
2026-05-28 16:37:35 +0200

    [BACKEND] Ajout variables environnement
    Ajout de variables et automatisation de leur récupération pour une création automatique du dossier result et centralisation des chemins.

--- compute_formations.py
+++ compute_formations.py
@@ -1,11 +1,18 @@
 import os
+from dotenv import load_dotenv
 from extractors.esf_csv import load_csv_data
 from transformers.rome_rncp_filtering import filter_data
 from utils.write_to_csv import write_dataframe
+from utils.create_folder import create_folder
+
+load_dotenv()
+
+result_path = os.getenv("RESULT_FOLDER_PATH")
+formations_path = os.getenv("FORMATIONS_PATH")
+correspondances_path = os.getenv("CORRESPONDANCES_PATH")
 
 def compute_formation_data():
-    formations_path = "result/formations.csv"
-    correspondances_path = "result/correspondances.csv"
+    create_folder(result_path)
     if os.path.exists(formations_path) and os.path.exists(correspondances_path):
         print(f"Données {formations_path} et {correspondances_path} existantes. Réécriture des données ignorée.")
         return
