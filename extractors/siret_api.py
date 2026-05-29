7f9f9992c167d24ba42096ffa815f76948bde8ec
pierre-jannot <pierre.jannot@isen.yncrea.fr>
2026-05-29 16:58:45 +0200

    [BACKEND] Ajout de la correspondance code Régions INSEE et noms Régions

--- extractors/siret_api.py
+++ extractors/siret_api.py
@@ -1,5 +1,26 @@
 import requests
 
+regions_table = {
+    "84": "Auvergne-Rhône-Alpes",
+    "27": "Bourgogne-Franche-Comté",
+    "53": "Bretagne",
+    "24": "Centre-Val de Loire",
+    "94": "Corse",
+    "44": "Grand Est",
+    "32": "Hauts-de-France",
+    "11": "Île-de-France",
+    "28": "Normandie",
+    "75": "Nouvelle-Aquitaine",
+    "76": "Occitanie",
+    "52": "Pays de la Loire",
+    "93": "Provence-Alpes-Côte d'Azur",
+    "01": "Guadeloupe",
+    "03": "Guyane",
+    "02": "Martinique",
+    "04": "La Réunion",
+    "06": "Mayotte"
+}
+
 def get_region_from_siret(siret: str) -> dict:
     r = requests.get(
         "https://recherche-entreprises.api.gouv.fr/search",
@@ -11,5 +32,5 @@ def get_region_from_siret(siret: str) -> dict:
         "siret": siret,
         "nom_raison_sociale": result["nom_raison_sociale"],
         "code_postal": siege["code_postal"],
-        "region":      siege["region"],
+        "region":      regions_table[siege["region"]],
     }
