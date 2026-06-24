e3c746360fab8ef64e86ed2bcf3d07616a8ae2fb
Morgane Farge <morganefarge69@gmail.com>
2026-06-24 12:18:01 +0200

    ajout rouge stats skills

---
+++ compute_stats.py
@@ -0,0 +1,13 @@
+import pandas as pd 
+from collections import Counter
+
+# On charge le fichier
+df = pd.read_csv("job_offers.csv")
+
+# On commence le traitement datas 
+def get_top_skills():
+    skills = df["skill_tags"].str.split(",").explode().str.strip()
+    skill_counts = Counter(skills)
+    top_skills = skill_counts.most_common(10)
+    return [{"skill": skill, "count": count} for skill, count in top_skills]
+
