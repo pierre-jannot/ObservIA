ff160120e477d9bd55d4cfc01ef432b45f6f8dcb
pierre-jannot <pierre.jannot@isen.yncrea.fr>
2026-06-24 16:55:32 +0200

    [PULL REQUEST] Ajout de paramètres aux routes API (#3)
    * [API] Ajout de routes et fonctions pour exploration dataframe
    
    * [API] Ajout route offres par département
    
    * [BACKEND] Ajout script de scraping de Freework et mise à jour routes API
    
    - Script d'automatisation du scraping de Freework permettant de récupérer les informations de toutes les offres françaises
    - Ajout et correction de routes API pour éviter les erreurs de région/département
    
    * [API] Changement du fonctionnement de la route formation
    
    Changement du fonctionnement de la route formation pour prendre en compte les entrées en formation et non le nombre de formations.
    
    * [API] Rajout de la colonne sortie totale et route API sorties de formation
    
    rome_rncp_filtering :
    - Ajout de la colonne "sorties_realisation_totale" pour prendre en compte le nombre de sorties
    
    main.py :
    - Modification de la route présentant le nombre d'entrées en formation par trimestre
    - Ajout de la route présentant le nombre de sorties en réalisation totale par trimestre
    
    * [API] Factorisation des routes, séparation en fichiers et mise en place routeurs
    
    * [BACKEND] Mise à jour données récupérées Freework
    
    * [BACKEND] Ajouts codes région pour faciliter le traitement
    
    * [API] Ajout paramètres routes zone
    
    Paramètres de zone permettant de filtrer pour certaines zones géographiques la recherche d'offres et de formations.

--- routers/freework.py
+++ routers/freework.py
@@ -1,13 +1,17 @@
 from utils.compute_dataframe import get_filtered_values, get_quarter_values, count_unique_values
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
 
 import pandas as pd
 
 router = APIRouter()
 
 @router.get("/offers-per-quarter")
-def get_offers_per_quarter():
+def get_offers_per_quarter(
+    zone: list[str] | None = Query(None)
+):
     dataframe = pd.read_csv("result/freework_offers.csv", sep=";", encoding="utf-8")
+    if zone:
+        dataframe = get_filtered_values(dataframe, "location", zone)
     dataframe = get_quarter_values(dataframe, "publication_date")
     dataframe = count_unique_values(dataframe, "quarter")
     result = [
