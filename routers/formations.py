e846ba0d2317cb63b03d089e884c10a075f78be2
pierre-jannot <pierre.jannot@isen.yncrea.fr>
2026-06-24 16:54:10 +0200

    [API] Ajout paramètres routes zone
    Paramètres de zone permettant de filtrer pour certaines zones géographiques la recherche d'offres et de formations.

--- routers/formations.py
+++ routers/formations.py
@@ -1,13 +1,21 @@
-from fastapi import APIRouter
-from utils.compute_dataframe import get_quarter_values, sum_values
+from fastapi import APIRouter, Query
+from utils.compute_dataframe import get_quarter_values, sum_values, get_filtered_values
 
 import pandas as pd
 
 router = APIRouter()
 
 @router.get("/entry-per-quarter")
-def get_formation_entry_per_quarter():
+def get_formation_entry_per_quarter(
+    zone: list[str] | None = Query(None)
+):
     dataframe = pd.read_csv("result/formations.csv", sep=";", encoding="utf-8")
+    if zone:
+        sirets = pd.read_csv("result/sirets.csv", sep=";", encoding="utf-8")
+        correspondance = dict(zip(sirets["siret"], sirets["departement"]))
+        dataframe["zone"] = dataframe["siret_of_contractant"].map(correspondance)
+        print(dataframe["zone"])
+        dataframe = get_filtered_values(dataframe, "zone", zone)
     dataframe = get_quarter_values(dataframe, "annee_mois")
     dataframe = sum_values(dataframe, "entrees_formation", "quarter")
     result = [
@@ -17,8 +25,16 @@ def get_formation_entry_per_quarter():
     return {"result": result}
 
 @router.get("/exit-per-quarter")
-def get_formation_exit_per_quarter():
+def get_formation_exit_per_quarter(
+    zone: list[str] | None = Query(None)
+):
     dataframe = pd.read_csv("result/formations.csv", sep=";", encoding="utf-8")
+    if zone:
+        sirets = pd.read_csv("result/sirets.csv", sep=";", encoding="utf-8")
+        correspondance = dict(zip(sirets["siret"], sirets["departement"]))
+        dataframe["zone"] = dataframe["siret_of_contractant"].map(correspondance)
+        print(dataframe["zone"])
+        dataframe = get_filtered_values(dataframe, "zone", zone)
     dataframe = get_quarter_values(dataframe, "annee_mois")
     dataframe = sum_values(dataframe, "sorties_realisation_totale", "quarter")
     result = [
