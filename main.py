5fc0c9fe837f70cd4b671472c6cc9aa8c5a399fd
pierre-jannot <pierre.jannot@isen.yncrea.fr>
2026-06-24 12:17:44 +0200

    Squashed commit of the following:
    commit 08f58e3be17cc7982318efd7af9a89d522801227
    Author: pierre-jannot <pierre.jannot@isen.yncrea.fr>
    Date:   Wed Jun 24 12:17:22 2026 +0200
    
        [BACKEND] Ajout script de scraping de Freework et mise à jour routes API
    
        - Script d'automatisation du scraping de Freework permettant de récupérer les informations de toutes les offres françaises
        - Ajout et correction de routes API pour éviter les erreurs de région/département
    
    commit 06e23c1d0b859e55e50416768a03b9073cee3bc0
    Author: pierre-jannot <pierre.jannot@isen.yncrea.fr>
    Date:   Tue Jun 23 16:54:53 2026 +0200
    
        [API] Ajout route offres par département
    
    commit d2fb70057ad1f88a1c2573ea9e05a82d7c0d78d1
    Author: pierre-jannot <pierre.jannot@isen.yncrea.fr>
    Date:   Tue Jun 23 16:43:47 2026 +0200
    
        [API] Ajout de routes et fonctions pour exploration dataframe

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-from compute_formations import compute_formation_data, compute_sirets_information, compute_departments_information
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from utils.compute_dataframe import get_filtered_values, get_quarter_values, get_unique_values, count_unique_values
+from compute_freework_offers import compute_freework_offers
+from compute_formations import compute_all
+
+import pandas as pd
+
+compute_all()
+compute_freework_offers()
 
 app = FastAPI(
     title="Tensions formations et offres d'emploi Tech IA",
@@ -19,11 +26,64 @@ app.add_middleware(
 def root():
     return {"status": "ok"}
 
-def main():
-    compute_formation_data()
-    compute_departments_information()
-    compute_sirets_information()
-    return
+@app.get("/formations-per-quarter")
+def get_offers_per_quarter():
+    dataframe = pd.read_csv("result/formations.csv", sep=";", encoding="utf-8")
+    dataframe = get_quarter_values(dataframe, "annee_mois")
+    dataframe = count_unique_values(dataframe, "quarter")
+    result = [
+    {"trimestre": str(index), "nombre_formations": int(value)}
+    for index, value in dataframe.items()
+    ]
+    return {"result": result}
 
-if __name__ == "__main__":
-    main()
+@app.get("/offers-per-quarter")
+def get_offers_per_quarter():
+    dataframe = pd.read_csv("result/freework_offers.csv", sep=";", encoding="utf-8")
+    dataframe = get_quarter_values(dataframe, "publication_date")
+    dataframe = count_unique_values(dataframe, "quarter")
+    result = [
+    {"trimestre": str(index), "nombre_offres_freework": int(value)}
+    for index, value in dataframe.items()
+    ]
+    return {"result": result}
+
+@app.get("/offers-per-department")
+def get_formations_per_department():
+    dataframe = pd.read_csv("result/freework_offers.csv", sep=";", encoding="utf-8")
+    departments = pd.read_csv("result/departments.csv", sep=";", encoding="utf-8")
+    dataframe = get_filtered_values(dataframe, "location", departments["nom_departement"])
+    dataframe = count_unique_values(dataframe, "location")
+    result = [
+    {"département": index, "nombre_offres_freework": int(value)}
+    for index, value in dataframe.sort_values(ascending=False).items()
+    ]
+    return {"result": result}
+
+@app.get("/offers-per-region")
+def get_formations_per_region():
+    dataframe = pd.read_csv("result/freework_offers.csv", sep=";", encoding="utf-8")
+    departments = pd.read_csv("result/departments.csv", sep=";", encoding="utf-8")
+    correspondance = dict(zip(departments["nom_departement"], departments["nom_region"]))
+    dataframe["region"] = dataframe["location"].map(correspondance).fillna(dataframe["location"])
+    dataframe = count_unique_values(dataframe, "region")
+    result = [
+    {"région": index, "nombre_offres_freework": int(value)}
+    for index, value in dataframe.sort_values(ascending=False).items()
+    ]
+    return {"result": result}
+
+
+@app.get("/offers-per-region/{region}")
+def get_formations_per_region(region: str):
+    dataframe = pd.read_csv("result/freework_offers.csv", sep=";", encoding="utf-8")
+    departments = pd.read_csv("result/departments.csv", sep=";", encoding="utf-8")
+    correspondance = dict(zip(departments["nom_departement"], departments["nom_region"]))
+    dataframe["region"] = dataframe["location"].map(correspondance).fillna(dataframe["location"])
+    dataframe = get_filtered_values(dataframe, "region", [region])
+    dataframe = count_unique_values(dataframe, "region")
+    result = [
+    {"région": index, "nombre_offres_freework": int(value)}
+    for index, value in dataframe.sort_values(ascending=False).items()
+    ]
+    return {"result": result}
--- utils/write_to_csv.py
+++ utils/write_to_csv.py
@@ -1,8 +1,13 @@
 import os
 
-def write_dataframe(path, dataframe):
-    if os.path.exists(path):
-        print(f"Données {path} existantes. Réécriture des données ignorée.")
+def write_dataframe(path, dataframe, method="w"):
+    if method == "w":
+        if os.path.exists(path):
+            print(f"Données {path} existantes. Réécriture des données ignorée.")
+        else:
+            dataframe.to_csv(path, sep=";", index=False, encoding="utf-8")
+            print(f"Données {path} écrites avec succès.")
+    elif method == "a":
+        dataframe.to_csv(path, mode="a", header=not os.path.exists(path), sep=";", index=False, encoding="utf-8")
     else:
-        dataframe.to_csv(path, sep=";", index=False, encoding="utf-8")
-        print(f"Données {path} écrites avec succès.")
+        print(f"Method {method} unknown.")
