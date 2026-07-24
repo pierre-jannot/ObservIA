72eed978dd52b630e07e1c73b699c13cd8866d5a
pierre-jannot <pierre.jannot@isen.yncrea.fr>
2026-07-24 11:08:02 +0200

    [FIX] URL BDD (#18)
    * [IN PROGRESS] Mise à jour base de données
    
    * [BDD] Mise à jour de la base de données et routes data, formations, freework fonctionnelles
    
    * [BDD] Insertion des données France Travail dans la nouvelle base de données
    
    * [ROUTES] Correction des repositories et routes pour faire fonctionner les routes France Travail
    
    * [CICD] Mise en docker de la database
    
    * [FIX] URL Database

--- db/session.py
+++ db/session.py
@@ -8,18 +8,7 @@ import dotenv
 
 dotenv.load_dotenv()
 
-#DATABASE_URL = os.getenv("DATABASE_URL")
-
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_NAME = os.getenv("DB_NAME")
-
-DATABASE_URL = (
-    f"postgresql+psycopg://{DB_USER}:{DB_PASSWORD}"
-    f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
